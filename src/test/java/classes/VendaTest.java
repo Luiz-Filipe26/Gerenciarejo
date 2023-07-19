aed01ede20ecfb9a75551f51f7a090131edc1b78
Gessyca Moreira <72661229+Geessyca@users.noreply.github.com>
2023-07-19 20:14:51 -0300

    Update VendaTest.java
    equals

--- src/test/java/classes/VendaTest.java
+++ src/test/java/classes/VendaTest.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 public class VendaTest {
 
     @Test
-    public void testVendaEquals() {
+     public void testVendaEquals() {
         Cliente cliente = new Cliente("123456789", "João", "123456789", "Rua A");
         Usuario usuario = new Usuario(1L, "Nome", "username1", "senha1", Perfil.ADMIN, LocalDateTime.now(), LocalDateTime.now());
         HashMap<String, VendaDetalhes> vendasDetalhes = new HashMap<>();
@@ -19,7 +19,9 @@ public class VendaTest {
         Venda venda2 = new Venda(1L, cliente, usuario, BigDecimal.valueOf(100.00), BigDecimal.valueOf(100.00), BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now(), LocalDateTime.now(), vendasDetalhes);
         Venda venda3 = new Venda(2L, cliente, usuario, BigDecimal.valueOf(200.00), BigDecimal.valueOf(200.00), BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now(), LocalDateTime.now(), vendasDetalhes);
         Assertions.assertEquals(venda1, venda2);
-        Assertions.assertNotEquals(venda1, venda3);
+        Assertions.assertTrue(venda1.equals(venda1)); 
+        Assertions.assertTrue(venda1.equals(venda2)); 
+        Assertions.assertFalse(venda1.equals(venda3)); 
     }
 
     @Test
